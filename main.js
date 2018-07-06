a6802cb505e049ad0402a779b1fe949ae5481f6a
Yuraist <yura.ist@icloud.com>
2018-07-06 08:47:32 +0300

    Added a new page and dropdown navbar

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggle() {
+  var x = document.getElementById('myTopnav')
+  if (x.className == 'topnav') {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
